7a505d6328d56398c31b87b86158bd9f728136ba
richard <richard.turra@sensedata.com.br>
2020-10-28 10:52:09 -0300

    test request csv, json, dataframes fields

--- flask/app/load_data.py
+++ flask/app/load_data.py
@@ -6,13 +6,31 @@ from .model import Client, Name, Location, Coordinates, Timezone, Picture
 from dataclasses import asdict
 
 
+def request_csv():
+    """ Make a request to a csv resource and return the file"""
+    resp_csv = requests.get("https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.csv")
+    return resp_csv
+
+def request_json():
+    """ Make a request to a json resource and return the file"""
+    resp_json = requests.get("https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.json")
+    return resp_json
+
+
 def dataframe_from_csv():
     """ Return a structured pandas dataframe from a requested csv file"""
-    resp_csv = requests.get("https://storage.googleapis.com/juntossomosmais-code-challenge/input-backend.csv")
-    s = str(resp_csv.content,'utf-8')
-    csv_file = StringIO(s)
-    df_csv = process_csv(pd.read_csv(csv_file))
+    response = request_csv()
+    s = str(response.content,'utf-8')
+    csv_content = StringIO(s)
+    df_csv = process_csv(pd.read_csv(csv_content))
     return df_csv
+
+
+def dataframe_from_json():
+    """ Return a structured pandas dataframe from a requested json file"""
+    response = request_json()
+    df_json = process_json(pd.DataFrame.from_dict(response.json()))
+    return df_json
 
 
 def process_csv(df):
@@ -34,13 +52,13 @@ def process_csv(df):
                 state= "",
                 postcode = 0,
                 coordinates = Coordinates(
-                    latitude = 0.0, 
+                    latitude = 0.0,
                     longitude = 0.0
                     ),
                 timezone= Timezone(
-                    offset = "", 
+                    offset = "",
                     description= ""
-                    ) 
+                    )
                 ),
             email = "",
             birthday = "",
@@ -60,6 +78,50 @@ def process_csv(df):
     new_df = pd.DataFrame.from_dict(obj_list)
     return new_df
 
+def process_json(df):
+    """Reestructure a json dataframe and return a new one with new fields"""
+    obj_list = []
+    for k, row in df.iterrows():
+        json_obj = asdict(Client(
+            type="",
+            gender = "",
+            name = Name(
+                title = "",
+                first = "",
+                last = ""
+                ),
+            location = Location(
+                region = "",
+                street = "",
+                city = "",
+                state= "",
+                postcode = 0,
+                coordinates = Coordinates(
+                    latitude = 0.0, 
+                    longitude = 0.0
+                    ),
+                timezone= Timezone(
+                    offset = "", 
+                    description= ""
+                    ) 
+                ),
+            email = "",
+            birthday = "",
+            registered = "",
+            telephoneNumbers = ["",""],
+            mobileNumbers = ["",""],
+            picture = Picture(
+                medium= "",
+                large="",
+                thumbnail=""
+                ),
+            nationality = ""
+            )
+        )
+        obj_list.append(json_obj)
+
+    new_df = pd.DataFrame.from_dict(obj_list)
+    return new_df
 
 def build_df():
     df_csv = dataframe_from_csv()
--- flask/tests/test_df.py
+++ flask/tests/test_df.py
@@ -1,5 +1,5 @@
 import json
-from app.load_data import dataframe_from_csv
+from app.load_data import dataframe_from_csv, dataframe_from_json, request_csv, request_json
 import pandas as pd
 
 json_model = {
@@ -42,22 +42,50 @@ json_model = {
   "nationality": "BR"
 }
 
+def test_request_csv():
+  """ Assert the csv resource response """
+  resp_csv = request_csv()
+  assert resp_csv.status_code == 200
 
-def test_df_csv():
-    """ Assert the every key of the model is present in the csv dataframe columns"""
-    df_csv = dataframe_from_csv()
+def test_request_json():
+  """ Assert the json resource response """
+  resp_json = request_json()
+  assert resp_json.status_code == 200
 
-    # assert that the response is a dataframe
-    assert isinstance(df_csv, pd.DataFrame)
 
-    # assert that every key is present in the dataframe columns
-    for key in json_model:
-        assert key in df_csv.columns
-        # assert nested keys in the json types
-        if isinstance(json_model[key], dict):
-          for k1 in json_model[key]:
-            assert k1 in df_csv[key].iloc[0]
-            if isinstance(json_model[key][k1], dict):
-              for k2 in json_model[key][k1]:
-                assert k2 in df_csv[key].iloc[0][k1]
+def test_dataframe_from_csv():
+  """ Assert the every key of the model is present in the csv dataframe columns"""
+  df_csv = dataframe_from_csv()
+
+  # assert that the response is a dataframe
+  assert isinstance(df_csv, pd.DataFrame)
+
+  # assert that every key is present in the dataframe columns
+  for key in json_model:
+      assert key in df_csv.columns
+      # assert nested keys in the json types
+      if isinstance(json_model[key], dict):
+        for k1 in json_model[key]:
+          assert k1 in df_csv[key].iloc[0]
+          if isinstance(json_model[key][k1], dict):
+            for k2 in json_model[key][k1]:
+              assert k2 in df_csv[key].iloc[0][k1]
+
+def test_dataframe_from_json():
+  """ Assert the every key of the model is present in the json dataframe columns"""
+  df_json = dataframe_from_json()
+
+  # assert that the response is a dataframe
+  assert isinstance(df_json, pd.DataFrame)
+
+  # assert that every key is present in the dataframe columns
+  for key in json_model:
+      assert key in df_json.columns
+      # assert nested keys in the json types
+      if isinstance(json_model[key], dict):
+        for k1 in json_model[key]:
+          assert k1 in df_json[key].iloc[0]
+          if isinstance(json_model[key][k1], dict):
+            for k2 in json_model[key][k1]:
+              assert k2 in df_json[key].iloc[0][k1]
         
